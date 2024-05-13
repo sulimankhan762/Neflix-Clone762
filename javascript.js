2c3eac3e2b6130f4fbb70a3e59ffb2b21972ae93
sulimankhan762 <123302055+sulimankhan762@users.noreply.github.com>
2024-05-13 16:34:39 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+let moviesContainer = document.querySelector(".pictures");
+
+
+function onMovieLeft()
+{
+    moviesContainer.style.left = "0%";
+}
+
+function onMovieRight()
+{
+    moviesContainer.style.left = "-120%";
+}
